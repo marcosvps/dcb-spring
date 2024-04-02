e526b19541b64fe21185cabeeee2bce2422b7c08
marcovps <marcosvporto@DESKTOP-7T07Q8L>
2024-04-01 23:34:49 -0300

    Implementing Business Logic - Department Service Complete

--- department-service/src/main/java/com/dailycodebuffer/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/com/dailycodebuffer/departmentservice/controller/DepartmentController.java
@@ -1,2 +1,40 @@
-package com.dailycodebuffer.departmentservice.controller;public class DepartmentController {
+package com.dailycodebuffer.departmentservice.controller;
+
+
+import com.dailycodebuffer.departmentservice.model.Department;
+import com.dailycodebuffer.departmentservice.repository.DepartmentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/department")
+public class DepartmentController {
+
+    private static final Logger LOGGER
+            = LoggerFactory.getLogger(DepartmentController.class);
+    @Autowired
+    private DepartmentRepository repository;
+
+    @PostMapping
+    public Department add(@RequestBody Department department) {
+        LOGGER.info("Department add:{}", department);
+        return repository.addDeparment(department);
+    }
+
+    @GetMapping
+    public List<Department> findAll() {
+        LOGGER.info("Department find");
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Department findById(@PathVariable Long id) {
+        LOGGER.info("Department find: id={}", id);
+        return repository.findById(id);
+    }
+
 }
--- department-service/src/main/java/com/dailycodebuffer/departmentservice/model/Employee.java
+++ department-service/src/main/java/com/dailycodebuffer/departmentservice/model/Employee.java
@@ -1,2 +1,5 @@
-package com.dailycodebuffer.departmentservice.model;public record Employee() {
+package com.dailycodebuffer.departmentservice.model;
+
+public record Employee(Long id, Long departmentId, String name, int age, String position) {
+
 }
